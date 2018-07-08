1e467ed0636db955138353c4738d02ff724dd1a1
zhihua-zheng <greengo@outlook.com>
2018-07-07 18:21:12 -0700

    optimized 'init_out_display.m'

--- fortran_training/exercises/ex4/init_out_display.m
+++ fortran_training/exercises/ex4/init_out_display.m
@@ -6,12 +6,12 @@
 % July 6 2018
 
 clear
-ny = 21; % grid points in y
-nx = 43; % grid points in x
+ny = 43; % grid points in y
+nx = 87; % grid points in x
 
 
-%% random
-clear
+%% initial random
+
 T = rand(ny,nx);
 T(1,:) = 0;
 T(end,:) = 0;
@@ -29,11 +29,11 @@ fprintf(fileID,'%5.2f\n',T);
 fclose(fileID);
 
 
-%% spike
+%% initial spike
 
-clear
+
 T = zeros(ny,nx);
-T(11,22) = 1;
+T((ny+1)/2,(nx+1)/2) = 1;
 
 figure('position', [0, 0, 800, 400])
 contourf(T)
@@ -46,10 +46,7 @@ fclose(fileID);
 
 %% plot the output
 
-fileID = fopen('/Users/Danny/Downloads/Practice/fortran_training/exercises/ex4/initial_T.dat','w');
-fprintf(fileID,'%5.2f\n',T);
-fclose(fileID);
-
+load '/Users/Danny/Downloads/Practice/fortran_training/exercises/ex4/final_T.dat'
 
 final_T = reshape(final_T, ny, nx);
 
